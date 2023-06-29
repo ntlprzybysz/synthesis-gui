dc2db53fb9d469466ed984fe3142b833046d7c02
Natalia Przybysz <ntl.przybysz@gmail.com>
2023-06-29 11:28:23 +0200

    Modelling project, allowed sessions middleware

--- synthesis/models.py
+++ synthesis/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Project:
+    """
+    Saves all data user provided about the project and provide a method to synthesize it.
+    """
+    def __init__(self, cleaned_form_input: dict) -> None:
+        self.name: str = cleaned_form_input["project_name"]
+        self.text_input: str = cleaned_form_input["text_input"]
+        self.ipa_input: str = cleaned_form_input["ipa_input"]
+        self.model: str = cleaned_form_input["model"]
+        self.voice: str = cleaned_form_input["voice"]
+        self.sentence: int = int(cleaned_form_input["sentence"])
+
+    def synthesise(self):  # -> audio_file:
+        """
+        Uses user's IPA input and settings to generate an audio file with synthesised sentence.
+        """
+        pass
--- synthesis/views.py
+++ synthesis/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
-#from django.http import HttpResponse
 
 from .forms import InputForm
+from .models import Project
 
 
 def show_home(request):
     if request.method == "POST":
         form = InputForm(request.POST)
         if form.is_valid(): # required fields filled in properly
-            project = start_project(form.cleaned_data)
+            project = Project(form.cleaned_data)
+            #assert project.name == "Example synthesis"
+            #assert project.text_input == ""
+            #return render(request, "test.html")
     else:
         form = InputForm()
 
@@ -21,47 +24,3 @@ def show_about(request):
 
 def show_help(request):
     return render(request, "help.html")
-
-
-class Settings:
-    """
-    Saves an object with all user settings needed for synthesis.
-    """
-
-    def __init__(self):
-        self.model: str = "LJSpeech 1.1"
-        self.voice: str = "Linda Johnson"
-        self.sentence: int = 0
-
-
-class Project:
-    """
-    Saves all data user provided about the poject.
-    """
-    def __init__(self):
-        self.name = "Example project"
-        self.text_input: str = ""
-        self.ipa_input: str = ""
-        self.settings: "Settings" = Settings()
-
-
-def start_project(cleaned_form_input: dict) -> "Project":
-    """
-    Creates a Project object from the data sent via the form.
-    """
-    project = Project()
-    project.name = cleaned_form_input["project_name"]
-    project.text_input = cleaned_form_input["text_input"]
-    project.ipa_input = cleaned_form_input["ipa_input"]
-    project.settings.model = cleaned_form_input["model"]
-    project.settings.voice = cleaned_form_input["voice"]
-    project.settings.sentence = int(cleaned_form_input["sentence"])
-
-    return project
-
-
-def synthesise(ipa_input: str, settings: "Settings"):  # -> audio_file:
-    """
-    Uses user's IPA input and settings to generate an audio file with synthesised sentence.
-    """
-    pass
