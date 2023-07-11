a798547be97ba4a1f62f80e7e43830f14f55eb9e
Natalia Przybysz <ntl.przybysz@gmail.com>
2023-07-11 10:22:36 +0200

    Introduced serving the audio file to the user (production), a download button and created a gitignore

--- synthesisgui/settings.py
+++ synthesisgui/settings.py
@@ -121,7 +121,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'synthesis' / 'static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'synthesis' / 'media'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
