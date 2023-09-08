fe560c85607684848626669d4934e3c85484920f
Natalia Przybysz <ntl.przybysz@gmail.com>
2023-09-08 09:16:39 +0200

    Introduced scheduled tasks with Celery

--- synthesis/tasks.py
+++ synthesis/tasks.py
@@ -1,6 +1,10 @@
 import logging
+import shutil
+from pathlib import Path
 
+from synthesisgui.celery import app
 from celery import shared_task
+from django.conf import settings
 from synthesis.models import Project
 
 # from celery_progress.backend import ProgressRecorder
@@ -19,3 +23,36 @@ def synthesize_with_celery(cleaned_form_input: dict, session_key: str) -> bool:
 
     logger.error(f"session key {session_key} Failed to create new project.")
     return False
+
+
+@app.task
+def clean_media_folder() -> None:
+    logger = logging.getLogger("django")
+
+    media_dir_path = Path(settings.MEDIA_ROOT)
+
+    try:
+        for item in media_dir_path.glob("*"):
+            if item.is_file():
+                item.unlink()
+            elif item.is_dir():
+                shutil.rmtree(item)
+
+        logger.info(f"Media directory '{media_dir_path}' cleared successfully.")
+
+    except Exception as e:
+        logger.warning(f"Failed to clear media directory '{media_dir_path}': {str(e)}")
+
+    except:
+        logger.warning(f"Failed to clear media directory '{media_dir_path}'")
+
+'''
+@app.task
+def print_scheduled_task_msg() -> None:
+    logger = logging.getLogger("django")
+    try:
+        logger.info(f"This is a scheduled test message.")
+
+    except Exception as e:
+        logger.warning(f"Failed to print a scheduled test message: {str(e)}")
+'''
--- synthesisgui/celery.py
+++ synthesisgui/celery.py
@@ -4,6 +4,7 @@ import os
 from logging.config import dictConfig
 
 from celery import Celery
+from celery.schedules import crontab
 from celery.signals import setup_logging
 from django.conf import settings
 
@@ -27,3 +28,19 @@ app.autodiscover_tasks()
 @app.task(bind=True)
 def debug_task(self):
     print("Request: {0!r}".format(self.request))
+
+
+# Removes content of media folder in background
+app.conf.beat_schedule = {
+    "scheduled-cleaning-media-folder": {
+        "task": "synthesis.tasks.clean_media_folder",
+        "schedule": crontab(minute=0, hour=0, day_of_week=4),  # Runs on Thursday at midnight
+    },
+    #"scheduled-test-task": {
+    #    "task": "synthesis.tasks.print_scheduled_task_msg",
+    #    "schedule": crontab(minute="*"),
+    #},
+
+}
+
+app.conf.timezone = "Europe/Berlin"
