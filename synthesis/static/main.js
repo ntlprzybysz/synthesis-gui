a903aa93cb6c39ea6e097379225e35f480684bf0
Natalia Przybysz <ntl.przybysz@gmail.com>
2023-09-20 08:59:27 +0200

    Added loading example

--- synthesis/static/main.js
+++ synthesis/static/main.js
@@ -138,6 +138,68 @@ function loadInput(file) {
 
 
 /**
+ * Populates input fields with the chosen example.
+ *
+ * @param {string} chosenExample - The selected example keyword.
+ */
+function loadExample(chosenExample) {
+    switch(chosenExample) {
+        case "quick":
+            document.getElementById("project-name-field").value = "Quick test";
+            document.getElementById("text-input-field").value = "printing";
+            document.getElementById("ipa-input-field").value = "p˘|r˘|ɪː|n˘|t|ɪː|ŋː";
+            document.getElementById("model-select-field").value = "LJSPEECH11";
+            document.getElementById("voice-select-field").value = "LINDAJOHNSON";
+            document.getElementById("sentence-select-field").value = 1;
+            break;
+
+        case "printing":
+            document.getElementById("project-name-field").value = "Example printing";
+            document.getElementById("text-input-field").value = "Printing, in the only sense with which we are at present concerned, differs from most if not from all the arts and crafts represented in the Exhibition in being comparatively modern.";
+            document.getElementById("ipa-input-field").value = "p˘|r˘|ɪː|n˘|t|ɪː|ŋː|,|SIL2|ɪː|n|SIL0|ð|əː|SIL1|ˈoʊ|n|l˘|i|SIL0|sː|ˈɛˑ|n|s|SIL0|w˘|ˈɪ˘|ðː|SIL0|w|ˈɪ|tʃ|SIL0|w|ˈi|SIL0|ˈɑ|r|SIL0|ˈæ|t|SIL0|p˘|r˘|ˈɛ|z|ə|n˘|t|SIL0|k˘|ə˘|n|s|ˈʌrː|n|dː|,|SIL3|d|ˈɪ|f|ərˑ|zː|SIL1|f˘|r˘|ˈʌ˘|mˑ|SIL0|m˘|ˈoʊˑ|s|t˘|SIL0|ˈɪ|f|SIL0|n|ˈɑ|t|SIL0|f˘|r˘|ˈʌ˘|m|SIL0|ˈɔ|lˑ|SIL0|ðˑ|əː|SIL0|ˈɑ|r|t|s˘|SIL0|ə|n|d|SIL0|k|r˘|ˈæ|f|t˘|s|SIL0|r|ˌɛ˘|p|r˘|ɪ˘|z|ˈɛ|n˘|t|ɪ|d|SIL0|ɪː|n|SIL0|ð˘|ə|SIL0|ˌɛˑ|k|s˘|ə|b|ˈɪˑ|ʃ|əˑ|nː|SIL2|ɪ|n|SIL0|b˘|ˈi˘|ɪː|ŋ˘|SIL0|k˘|ə˘|m|p|ˈɛr|ə|t|ɪ|v|l|i|SIL0|mˑ|ˈɑː|d|";
+            document.getElementById("model-select-field").value = "LJSPEECH11";
+            document.getElementById("voice-select-field").value = "LINDAJOHNSON";
+            document.getElementById("sentence-select-field").value = 1;
+            break;
+        
+        case "mountain":
+            document.getElementById("project-name-field").value = "Example mountain";
+            document.getElementById("text-input-field").value = "As the overlying plate lifts up, it also forms mountain ranges.";
+            document.getElementById("ipa-input-field").value = "ˈæ|z|SIL0|ð|ʌ|SIL0|ˌoʊ|v|ɝ|l|ˈaɪ|ɪ|ŋ|SIL0|p|l|ˈeɪ|t|SIL0|l|ˈɪ|f|t|s|SIL0|ˈʌ|p|,|SIL1|ɪ|t|SIL0|ˈɔ|l|s|oʊ|SIL0|f|ˈɔ|ɹ|m|z|SIL0|m|ˈaʊ|n|t|ʌ|n|SIL0|ɹ|ˈeɪ|n|d͡ʒ|ʌ|z|.|SIL2";
+            document.getElementById("model-select-field").value = "LJSPEECH11";
+            document.getElementById("voice-select-field").value = "LINDAJOHNSON";
+            document.getElementById("sentence-select-field").value = 1;
+            break;
+        
+        case "wind":
+            document.getElementById("project-name-field").value = "Example north wind";
+            document.getElementById("text-input-field").value = "The North Wind and the Sun were disputing which was the stronger, when a traveler came along wrapped in a warm cloak.";
+            document.getElementById("ipa-input-field").value = "ð|ʌ|SIL0|n|ˈɔ|ɹ|θ|SIL0|w|ˈɪ|n|d|SIL0|ˈæ|n|d|SIL0|ð|ʌ|SIL0|s|ˈʌ|n|SIL0|w|ɝ|SIL0|d|ɪ|s|p|j|ˈu|t|ɪ|ŋ|SIL0|h|w|ˈɪ|t͡ʃ|SIL0|w|ˈɑ|z|SIL0|ð|ʌ|SIL0|s|t|ɹ|ˈɔ|ŋ|ɝ|,|SIL1|h|w|ˈɛ|n|SIL0|ʌ|SIL0|t|ɹ|ˈæ|v|ʌ|l|ɝ|SIL0|k|ˈeɪ|m|SIL0|ʌ|l|ˈɔ|ŋ|SIL0|ɹ|ˈæ|p|t|SIL0|ɪ|n|SIL0|ʌ|SIL0|w|ˈɔ|ɹ|m|SIL0|k|l|ˈoʊ|k|.|SIL2";
+            document.getElementById("model-select-field").value = "LJSPEECH11";
+            document.getElementById("voice-select-field").value = "LINDAJOHNSON";
+            document.getElementById("sentence-select-field").value = 1;
+            break;
+
+        default:
+            document.getElementById("project-name-field").value = "Example synthesis";
+            document.getElementById("text-input-field").value = "";
+            document.getElementById("ipa-input-field").value = "";
+            document.getElementById("model-select-field").value = "LJSPEECH11";
+            document.getElementById("voice-select-field").value = "LINDAJOHNSON";
+            document.getElementById("sentence-select-field").value = 1;
+    };
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("example").addEventListener("change", function () {
+        var selectedValue = document.getElementById("example").value;
+        loadExample(selectedValue);
+    });
+});
+
+
+
+/**
  * Allows the user to select and load a project file from their local system.
  */
 function loadProject() {
